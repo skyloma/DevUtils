966584df2d9609ed39b81c920ea82163a7a96923
lomasky <271341794@qq.com>
2016-07-13 14:21:57 +0800

    init

--- app/src/main/java/sample/devutils/MainActivity.java
+++ app/src/main/java/sample/devutils/MainActivity.java
@@ -1,18 +1,23 @@
 package sample.devutils;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
+import lomasky.ma.utils.DeviceUtils;
 import lomasky.ma.utils.SharedPrefUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +25,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         SharedPrefUtil.putString(this,"zjt","ok");
+        for (String s : DeviceUtils.getAppPackageNamelist(this)) {
+            Log.i("zjt",s);
+        }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPrefUtil.remove(MainActivity.this,"zjt");
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+              dialog=  ProgressDialog.show(MainActivity.this, // context
+                        "", // title
+                        "Loading. Please wait...", // message
+                        true,true); //进度是
+
             }
         });
     }
@@ -48,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            dialog.cancel();
             return true;
         }
 
